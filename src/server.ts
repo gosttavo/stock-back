bba8e1234d1b27ae8a9d044dac02140ccd0c0dbd
gus <dev@ninsaude.com>
2024-06-18 00:38:42 -0300

    adc logs de teste

--- src/server.ts
+++ src/server.ts
@@ -16,6 +16,13 @@ app.use(routers);
 const port = Number(process.env.GLOBAL_PORT) || 3000;
 
 AppDataSource.initialize().then(async () => {
+
+    console.log(`Configured port: ${port}`);
+    console.log(`Environment: ${process.env.NODE_ENV}`);
+    console.log(`DB Host: ${process.env.DB_HOST}`);
+    console.log(`DB User: ${process.env.DB_USER}`);
+    console.log(`DB Name: ${process.env.DB_NAME}`);
+
     app.listen(port, '0.0.0.0', () => {
         if (process.env.NODE_ENV === 'dev') {
             console.log(`Server running on port ${port}.`);
